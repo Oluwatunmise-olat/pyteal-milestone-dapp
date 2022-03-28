2e8eca69b1171d5dcb5886b67ab8ddf71df955a3
Oluwatunmise <oluwatunmiseolatunbosun2001@gmail.com>
2022-03-28 17:03:44 +0100

    Add set state function

--- src/contract.py
+++ src/contract.py
@@ -42,6 +42,8 @@ def approval_program():
             App.globalPut(global_sent, Bytes("False")), # the paymnet hasn't been made
             App.globalPut(global_submission_date, Int(0)), # No submission date has been set
             App.globalPut(global_submitted, Bytes("False")), # milestone hasn't been submitted yet. 
+            App.globalPut(global_start_date, Int(0)), # set the start date to Int(0)
+            App.globalPut(global_end_date, Int(0)), # set the end date to Int(0)
             Approve()
         ])
 
@@ -51,7 +53,31 @@ def approval_program():
 
     @Subroutine(TealType.none)
     def set_state():
-        pass
+        # This fucntion sets the parameters to get the contract started
+        # It is a grouped transaction with the second being a payment transaction to the smart contract escrow address.
+
+        return Seq([
+            # Run some checks
+            Assert(
+                And(
+                    Global.group_size() == Int(2), # assert it a group transaction with a group size of 2
+                    Txn.group_index() == Int(0),
+                    Txn.sender() == App.globalGet(global_client), # assert it is the client making this call
+                    Txn.application_args.length() == Int(3), # assert the length of the argumnets passed is 3
+
+                    Gtxn[1].type_enum() == TxnType.Payment, # assert the second transaction in the group is a payment transaction
+                    Gtxn[1].amount() == App.globalGet(global_amount), # assert the right amount is sent
+                    Gtxn[1].receiver() == Global.current_application_address(), # assert the payment is to the smart contract escrow address
+                    Gtxn[1].close_to_receiver() == Global.zero_address(),
+                    App.globalGet(global_start_date) == App.globalGet(global_end_date) == Int(0), # assert the contract hasn't started
+                )
+            ),
+
+            #  set the start and end dates
+            App.globalPut(global_start_date, Btoi(Txn.application_args[1])),
+            App.globalPut(global_end_date, Btoi(Txn.application_args[2])),
+            Approve()
+        ])
 
     @Subroutine(TealType.none)
     def accept():
