04888c470c6b780b6668e8cfa783be1fbcb3a810
Oluwatunmise <oluwatunmiseolatunbosun2001@gmail.com>
2022-03-29 16:13:36 +0100

    deployemnt code almost ready ...

--- src/deploy.py
+++ src/deploy.py
@@ -6,6 +6,16 @@ from utils.services import WebService, AccountService, transaction_instance
 from contract import approval_program, clear_program
 from deploy_helpers import contract_schema, compile_to_bytes
 
+# Addresses
+client_address = WebService.get_address_from_pk(
+    AccountService.get_client_private_keyy)
+freelancer_address = WebService.get_address_from_pk(
+    AccountService.get_freelancer_private_key)
+
+# Private Keys
+client_pk = AccountService.get_client_private_key
+freelancer_pk = AccountService.get_freelancer_private_key
+
 
 class Interface:
 
@@ -22,22 +32,22 @@ class Interface:
 
         # arguments
         args = [
-            WebService.get_address_from_pk(AccountService.get_client_private_key),
-            1_500_000, # (15 algo)
-            WebService.get_address_from_pk(AccountService.get_freelancer_private_key)
+            client_address,
+            1_500_000,  # (15 algo)
+            freelancer_address
         ]
 
         app_id = transaction_instance.create_contract(
-            approval_program= compiled_approval_program,
-            clear_program= compiled_clear_program,
-            global_schema= global_schema,
-            local_schema= local_schema,
+            approval_program=compiled_approval_program,
+            clear_program=compiled_clear_program,
+            global_schema=global_schema,
+            local_schema=local_schema,
             args=args
         )
 
         # Read global state of contract
 
-        global_state=transaction_instance.read_global_state(app_id)
+        global_state = transaction_instance.read_global_state(app_id)
 
         print("================================")
         print(" Samrt Contract Global State ...")
@@ -48,24 +58,37 @@ class Interface:
         return app_id
 
     @staticmethod
-    def set_up_call():
-        pass
+    def set_up_call(app_id, sender, sender_pk, receiver, amount):
+        args = [
+            "set_state",
+            "",  # start milestone timestamp
+            ""  # end milestone timestamp
+        ]
+
+        return transaction_instance.set_up_call(app_id=app_id, app_args=args, receiver=receiver, sender=sender, amount=amount, sender_pk=sender_pk)
 
     @staticmethod
-    def refund_call():
-        pass
+    def refund_call(sender_pk, sender, app_id, args):
+        return transaction_instance.refund_call(sender_pk=sender_pk, sender=sender, app_id=app_id, args=args)
 
     @staticmethod
-    def submit_call():
-        pass
+    def submit_call(app_id, sender_pk, sender):
+        args = [
+            ["submit"],
+            "True",
+            ""  # timestamp for altimatum (7 days from submission)
+        ]
+        return transaction_instance.submit_call(app_id=app_id, sender_pk=sender_pk, sender=sender, args=args)
 
     @staticmethod
-    def accept_call():
-        pass
+    def accept_call(app_id, sender_pk, sender, accounts: list):
+        args = ["accept"]
+        return transaction_instance.accept_call(app_id=app_id, sender_pk=sender_pk, sender=sender, accounts=accounts, args=args)
 
     @staticmethod
-    def withdraw_call():
-        pass
+    def withdraw_call(app_id, sender_pk, sender):
+        args = ["withdraw"]
+        return transaction_instance. withdraw_call(app_id=app_id, sender=sender, sender_pk=sender_pk, args=args)
 
     @staticmethod
     def decline_call():
@@ -96,7 +119,8 @@ def main():
         print("making set up call ...")
         print("======================")
 
-        set_up_response = Interface.set_up_call()
+        set_up_response = Interface.set_up_call(
+            receiver=smart_contract_address, app_id=application_id, amount=1_500_000, sender=client_address, sender_pk=client_pk)
 
         print(f"Smart Contract SetUp response {set_up_response}")
 
@@ -108,7 +132,8 @@ def main():
         print("making submit call ...")
         print("======================")
 
-        submit_response = Interface.submit_call()
+        submit_response = Interface.submit_call(
+            app_id=application_id, sender_pk=freelancer_pk, sender=freelancer_address)
         print(f"Smart Contract Submit response {submit_response}")
 
         time.sleep(15)
@@ -118,7 +143,8 @@ def main():
         print("making accept call ...")
         print("======================")
 
-        accept_response = Interface.accept_call()
+        accept_response = Interface.accept_call(
+            app_id=application_id, sender_pk=client_pk, sender=client_address, accounts=[freelancer_address])
         print(f"Smart Contract Accept response {accept_response}")
 
     except Exception as e:
